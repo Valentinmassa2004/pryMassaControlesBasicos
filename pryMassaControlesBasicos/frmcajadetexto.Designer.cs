226114d90f9d8bab4ff1bdf859ed36196b54d83f
root <mgiraudo@ies21.edu.ar>
2023-07-05 10:43:20 -0300

    .

--- pryMassaControlesBasicos/frmcajadetexto.Designer.cs
+++ pryMassaControlesBasicos/frmcajadetexto.Designer.cs
@@ -29,47 +29,23 @@
         private void InitializeComponent()
         {
             this.lblbienvenidos = new System.Windows.Forms.Label();
-            this.lblEjeY = new System.Windows.Forms.Label();
-            this.lblEjeX = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblbienvenidos
             // 
             this.lblbienvenidos.AutoSize = true;
-            this.lblbienvenidos.Font = new System.Drawing.Font("Ravie", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblbienvenidos.Location = new System.Drawing.Point(49, 74);
+            this.lblbienvenidos.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblbienvenidos.Location = new System.Drawing.Point(33, 59);
             this.lblbienvenidos.Name = "lblbienvenidos";
-            this.lblbienvenidos.Size = new System.Drawing.Size(896, 129);
+            this.lblbienvenidos.Size = new System.Drawing.Size(205, 30);
             this.lblbienvenidos.TabIndex = 1;
             this.lblbienvenidos.Text = "BIENVENIDOS";
             this.lblbienvenidos.Click += new System.EventHandler(this.lblbienvenidos_Click);
             // 
-            // lblEjeY
-            // 
-            this.lblEjeY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblEjeY.AutoSize = true;
-            this.lblEjeY.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.lblEjeY.Location = new System.Drawing.Point(12, 329);
-            this.lblEjeY.Name = "lblEjeY";
-            this.lblEjeY.Size = new System.Drawing.Size(14, 13);
-            this.lblEjeY.TabIndex = 1;
-            this.lblEjeY.Text = "Y";
-            // 
-            // lblEjeX
-            // 
-            this.lblEjeX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblEjeX.AutoSize = true;
-            this.lblEjeX.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.lblEjeX.Location = new System.Drawing.Point(12, 354);
-            this.lblEjeX.Name = "lblEjeX";
-            this.lblEjeX.Size = new System.Drawing.Size(14, 13);
-            this.lblEjeX.TabIndex = 0;
-            this.lblEjeX.Text = "X";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 295);
+            this.button1.Location = new System.Drawing.Point(89, 159);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -81,10 +57,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 366);
+            this.ClientSize = new System.Drawing.Size(280, 263);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblEjeX);
-            this.Controls.Add(this.lblEjeY);
             this.Controls.Add(this.lblbienvenidos);
             this.Name = "frmcajadetexto";
             this.Text = "principal";
@@ -96,8 +70,6 @@
 
         #endregion
         private System.Windows.Forms.Label lblbienvenidos;
-        private System.Windows.Forms.Label lblEjeY;
-        private System.Windows.Forms.Label lblEjeX;
         private System.Windows.Forms.Button button1;
     }
 }
